7dcf6935167089d464df305dc55082527d9cf138
isuman <ice_ass_whole@hotmail.com>
2017-08-01 11:52:12 +0700

    statistics fixed

--- index.android.js
+++ index.android.js
@@ -332,7 +332,7 @@ const styles = StyleSheet.create({
         backgroundColor: '#fff',
         borderColor: 'rgba(0,0,0,0.1)',
         margin: 5,
-        height: 300,
+        height: 280,
         padding: 15,
         shadowColor: '#ccc',
         shadowOffset: { width: 2, height: 2, },
